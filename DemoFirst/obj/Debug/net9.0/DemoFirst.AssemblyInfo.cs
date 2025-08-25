a6019363433f61a2f0a05d1c0bd4668da6926a09
OleksandrDarchyk <oledar01@easv365.dk>
2025-08-25 21:08:32 +0200

    feat: add initial project structure with React client and .NET server (dataaccess + service)

--- DemoFirst/obj/Debug/net9.0/DemoFirst.AssemblyInfo.cs
+++ DemoFirst/obj/Debug/net9.0/DemoFirst.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fbbf6535a3791a219c829f7eaf13029e062e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
